81a1e6a983f1fd12630f967b86d229ebc343dab3
tonynowater <tonynowater87@gmai..com>
2022-06-12 17:35:03 +0800

    Initial commit

---
+++ app/src/test/java/com/tonynowater/mi_notification_intercepter/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.tonynowater.mi_notification_intercepter
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
