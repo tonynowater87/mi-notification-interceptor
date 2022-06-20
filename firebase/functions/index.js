08e85bf4668fa380b738211e27863c4e9387e9f7
tonynowater <tonynowater87@gmai..com>
2022-06-20 21:58:06 +0800

    Use line-notify-sdk to replace line-message-api because of count limitation (500/month)

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -10,6 +10,10 @@ require('dotenv').config()
 const express = require('express');
 const line = require('@line/bot-sdk');
 
+// LINE Notify SDK
+const lineNotifySDK = require('line-notify-sdk');
+const lineNotify = new lineNotifySDK();
+
 // Config
 const config = {
   channelAccessToken: process.env.LINEBOT_CHANNEL_ACCESS_TOKEN,
@@ -20,6 +24,7 @@ const lineClient = new line.Client(config);
 line.middleware(config);
 
 const app = express();
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   functions.logger.log("LineMessageAPI webhook request", req);
   functions.logger.log("LineMessageAPI webhook response", res);
@@ -56,11 +61,11 @@ async function handleEvent(event) {
 }
 
 exports.pushMessage = functions.https.onRequest(async (req, res) => {
-  functions.logger.log('reg.body', req.body);
+  functions.logger.log('pushMessage reg.body', req.body);
 
   const type = req.body.data.type;
 
-  functions.logger.log(`message query = ${type}, ${timestamp}`);
+  functions.logger.log(`message body = ${type}, ${timestamp}`);
 
   var timestamp = admin.firestore.FieldValue.serverTimestamp()
 
@@ -93,12 +98,28 @@ exports.pushMessage = functions.https.onRequest(async (req, res) => {
         type: 'text',
         text: '阿嬤疑似於睡覺時間(00:00～06:30)從三樓走下二樓'
       };
+      break;    
+    case 'RoomDoorNotClosed':
+      console.log('阿嬤房門逾時未關，風扇關閉')
+      message = {
+        type: 'text',
+        text: '阿嬤房門逾時未關，關閉電風扇'
+      };
       break;
     default:
       console.log('TODO')
   }
 
-  groupRef.get().then(function (querySnapshot) {
+  lineNotify.notify(process.env.LINE_NOTIFY_TOKEN, message.text).then((body) => {
+    functions.logger.log(body);
+    response.status(200).json({ status: 200, message: 'ok' });
+  }).catch((e) => {
+    functions.logger.log(e);
+    response.status(500).json({ message: e });
+  });
+
+  // send message by group id
+  /* groupRef.get().then(function (querySnapshot) {
     querySnapshot.forEach(function (doc) {
       var groupId = doc.data().group;
       functions.logger.log(`push message, doc.data = ${groupId}`);
@@ -112,7 +133,7 @@ exports.pushMessage = functions.https.onRequest(async (req, res) => {
           res.status(500).json({ result: `Pushed Message Failed` });
         });
     });
-  })
+  }) */
 });
 
 // exports express middleware
@@ -126,32 +147,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.status(200).send({ data: "Hello from Firebase!!!!!" });
 });
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  functions.logger.log('reg.body', req.body);
-  //const body = JSON.parse(req.body);
-  const original = req.body.data.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({ original: original });
-  // Send back a message that we've successfully written the message
-  res.status(200).json({ result: `Message with ID: ${writeResult.id} added.` });
-});
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-  .onCreate((snap, context) => {
-    // Grab the current value of what was written to Firestore.
-    const original = snap.data().original;
-
-    // Access the parameter `{documentId}` with `context.params`
-    functions.logger.log('Uppercasing', context.params.documentId, original);
-
-    const uppercase = original.toUpperCase();
-
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return snap.ref.set({ uppercase }, { merge: true });
+exports.notifySticker = functions.https.onRequest((request, response) => {
+  functions.logger.log('notifySticker reg.body', request.body);
+  // Send a message with sticker
+  // https://developers.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
+  lineNotify.notify(process.env.LINE_NOTIFY_TOKEN, '出門嘍', '', '', 446, 1988).then((body) => {
+    functions.logger.log(body);
+    response.status(200).json({ status: 200, message: 'ok' });
+  }).catch((e) => {
+    functions.logger.log(e);
+    response.status(500).json({ message: e });
   });
+});
